4aa17ac0fa8cb2d67c82a90831cc4ca77809b7ca
Walter Chang <weihsiu@gmail.com>
2021-02-06 18:03:01 +0000

    more exs

--- tut/0-rustlings/exercises/conversions/from_into.rs
+++ tut/0-rustlings/exercises/conversions/from_into.rs
@@ -18,7 +18,6 @@ impl Default for Person {
     }
 }
 
-// I AM NOT DONE
 // Your task is to complete this implementation
 // in order for the line `let p = Person::from("Mark,20")` to compile
 // Please note that you'll need to parse the age component into a `usize`
@@ -34,6 +33,17 @@ impl Default for Person {
 // Otherwise, return an instantiated Person onject with the results
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut ps = s.split(',');
+        match ps.next() {
+            Some(name) => match ps.next() {
+                Some(n) => match n.parse::<usize>() {
+                    Ok(age) => Person { name: name.to_string(), age },
+                    _ => Default::default()
+                },
+                _ => Default::default()
+            },
+            _ => Default::default()
+        }
     }
 }
 
